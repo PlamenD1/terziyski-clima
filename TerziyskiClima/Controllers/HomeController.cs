0ca6e2e0bc8b0d940604f9d6dcf55a06661224c8
Plamen Dardzhikov <plamend1@abv.bg>
2022-03-31 21:32:44 +0300

    Styling for Home/Index.cshtml

--- TerziyskiClima/Controllers/HomeController.cs
+++ TerziyskiClima/Controllers/HomeController.cs
@@ -5,21 +5,28 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using TerziyskiClima.Data.Models;
 using TerziyskiClima.Models;
+using TerziyskiClima.Services;
 
 namespace TerziyskiClima.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ProductService productService;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, ProductService _productService)
         {
             _logger = logger;
+            productService = _productService;
         }
 
         public IActionResult Index()
         {
+            List<Product> featuredProducts = productService.GetProducts();
+            ViewBag.FeaturedProducts = featuredProducts;
             ViewBag.LoggedIn = HttpContext.User.Claims.Count()>0;
             if(ViewBag.LoggedIn)
                ViewBag.Email = HttpContext.User.Claims.ToList()[1].Value;
